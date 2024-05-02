a7e49671801797dd5a4db35ce7bb7ce04f2cbc6d
yanagiseira92717 <152665352+yanagiseira92717@users.noreply.github.com>
2024-05-02 09:16:21 +0900

    関数の修正

--- 関数/lesson2.php
+++ 関数/lesson2.php
@@ -4,28 +4,41 @@
 // 実行するとその都度現在の日本の日時に合わせて出力されるされるようになればOKです。
 // 日時がおかしい場合、PHPのタイムゾーン設定を確認して下さい。
 
-
 // ・現在日時（xxxx年xx月xx日（x曜日））
 // ・現在日時から３日後（yyyy年mm月dd日 H時i分s秒）
 // ・現在日時から１２時間前（yyyy年mm月dd日 H時i分s秒）
 // ・2020年元旦から現在までの経過日数 (ddd日)
 date_default_timezone_set('Asia/Tokyo');
 
-// 現在日時（xxxx年xx月xx日（x曜日））
-$currentDateTime = date('Y年m月d日（D）');
+echo "現在日時";
+$currentDateTime = date('(Y年m月d日（') . getJapaneseDayOfWeek(date('l')) . '）)';
 echo $currentDateTime . "<br>";
 
-// 現在日時から３日後（yyyy年mm月dd日 H時i分s秒）
-$threeDaysLater = date('Y年m月d日 H時i分s秒', strtotime('+3 days'));
+echo "現在日時から3日後";
+$threeDaysLater = date('(Y年m月d日 H時i分s秒)', strtotime('+3 days'));
 echo $threeDaysLater . "<br>";
 
-// 現在日時から１２時間前（yyyy年mm月dd日 H時i分s秒）
-$twelveHoursBefore = date('Y年m月d日 H時i分s秒', strtotime('-12 hours'));
+echo "現在日時から12時間前" ;
+$twelveHoursBefore = date('(Y年m月d日 H時i分s秒)', strtotime('-12 hours'));
 echo $twelveHoursBefore . "<br>";
 
-// 2020年元旦から現在までの経過日数 (ddd日)
+echo "2020年元旦から現在までの経過日数 ";
 $startDate = strtotime('2020-01-01');
 $currentDate = time();
 $elapsedDays = floor(($currentDate - $startDate) / (60 * 60 * 24));
-echo $elapsedDays . '日' . "<br>";
+echo "（" . $elapsedDays . '日）' . "<br>";
+
+function getJapaneseDayOfWeek($dayOfWeek) {
+    $dayOfWeekTranslations = array(
+        'Monday' => '月曜日',
+        'Tuesday' => '火曜日',
+        'Wednesday' => '水曜日',
+        'Thursday' => '木曜日',
+        'Friday' => '金曜日',
+        'Saturday' => '土曜日',
+        'Sunday' => '日曜日'
+    );
+
+    return $dayOfWeekTranslations[$dayOfWeek];
+}
 ?>
